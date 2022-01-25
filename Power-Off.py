6d476bac3f643d27adf30aeea58001500a435072
GeekSpeakTeam <76620649+GeekSpeakTeam@users.noreply.github.com>
2022-01-25 21:54:27 +0000

    Update Power-Off.py

--- Power-Off.py
+++ Power-Off.py
@@ -9,7 +9,6 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 hostname = '10.10.10.20'
 username = 'admin'
 password = 'Passw0rd01'
-
 ssh.connect(hostname=hostname,username=username, password=password,look_for_keys=False, allow_agent=False)
 
 connection = ssh.invoke_shell()
@@ -18,9 +17,9 @@ connection.send("top\r")
 sleep(.5)
 connection.send("scope chassis\r")
 sleep(.5)
-#connection.send("power off\r
+connection.send("power off\r")
 sleep(.5)
-#connection.send("y\r")
+connection.send("y\r")
 sleep(.5)
 connection.send("exit\r")
 sleep(.5)
